f647755d525336dfda6eede5aa85b93463b90209
Parth Gupta <gparth02@hotmail.com>
2022-12-07 17:06:37 -0800

    Added skeleton of Banker.cpp

--- Banker.cpp
+++ Banker.cpp
@@ -1,1 +1,42 @@
+#include "Banker.h"
 
+int main()
+{
+  return 0;
+}
+
+bool Banker::read(ifstream inputFile)
+{
+}
+
+bool Banker::execute()
+{
+}
+
+bool Banker::openAccount(std::string firstName, std::string lastName, std::string ID)
+{
+}
+
+bool Banker::viewHistory(const Account &a)
+{
+}
+
+bool Banker::viewHistory(const Account &a, int fund)
+{
+}
+
+bool Banker::transferFunds(Account &a, int fund1, int fund2)
+{
+}
+
+bool Banker::transferFunds(Account &a, int fundA, Account &b, int fundB, int amount)
+{
+}
+
+bool Banker::deposit(Account &a, int fund, int amount)
+{
+}
+
+bool Banker::withdraw(Account &a, int fund, int amount)
+{
+}
--- Banker.h
+++ Banker.h
@@ -6,7 +6,7 @@
 #include "AccountTree.h"
 // #include "TransactionReader.h"
 #include "Transaction.h"
-#include "Account.h"
+// #include "Account.h"
 #include <queue>
 #include <fstream>
 #include <iostream>
@@ -23,6 +23,7 @@ public:
 	bool execute();
 	bool openAccount(std::string firstName, std::string lastName, string ID);
 	bool viewHistory(const Account &a);
+	bool viewHistory(const Account &a, int fund);
 	bool transferFunds(Account &a, int fund1, int fund2);
 	bool transferFunds(Account &a, int fundA, Account &b, int fundB, int amount);
 	bool deposit(Account &a, int fund, int amount);
