8231a270c4a6d5ff21c47da27cddb8b37966fb11
Alex C <97079210+alexc405@users.noreply.github.com>
2022-12-07 18:04:36 -0800

    Update Transaction.h

--- Transaction.h
+++ Transaction.h
@@ -12,42 +12,47 @@ using namespace std;
 
 class Transaction
 {
-    friend ostream & operator<<(ostream &out, const Transaction &trans);
+private:
+    string firstName;
+    string lastName;
+    char transactionType;
+    int accountID;
+    int fundID;
+    int transferToAccountID;
+    int transferFundID;
+    int amount;
+    string fail;
 
 public:
     //constructor/destructor
     Transaction();
     ~Transaction();
 
-    Transaction(char type, string firstName, string lastName, int accountNumber);
+    Transaction(char typeOfTransaction, string firstName, string lastName, int accountNumber);
 
-    Transaction(char type, int accountNumber);
-    Transaction(char type, int accountNumber, int fundNumber);
-    Transaction(char type, int accountNumber, int fundNumber, int amount);
+    Transaction(char typeOfTransaction, int accountNo);
+    Transaction(char typeOfTransaction, int accountNo, int fundNo);
+    Transaction(char typeOfTransaction, int accountNo, int fundNo, int amount);
     //transaction that fails to process
-    Transaction(char type, int accountNumber, int fundNumber, int amount, string fail);
+    Transaction(char typeOfTransaction, int accountNo, int fundNo, int amount, string failTran);
 
-    Transaction(char type, int accountNumber, int fundNumber, int amount, int transferAccountNumber, int transferFundNumber);
+    Transaction(char typeOfTransaction, int accountNo, int fundNo, int amount,
+                int transferToAccount, int transferToFund);
     //transaction that fails to process
-    Transaction(char type, int accountNumber, int fundNumber, int amount, int transferAccountNumber, int transferFundNumber, string failString);
+    Transaction(char typeOfTransaction, int accountNo, int fundNo, int amount,
+                int transferToAccount, int transferToFund, string failTran);
 
     //getters
     char getTransactionType() const;
     string getFirstName() const;
     string getLastName() const;
     int getAccountID() const;
-    int getTransferAccountID() const;
+    int getTransferToAccountID() const;
     int getFundID() const;
-    int getTransferFundID() const;
+    int getTransferToFundID() const;
     int getAmount() const;
-    bool failed();
 
-private:
-    char transactionType;
-    string firstName;
-    string lastName;
-    string fail;
-    int accountID, fundID, transferAccountID, transferFundID, amount;
+    bool failedTransaction();
 };
 
 #endif //TRANSACTION_H
