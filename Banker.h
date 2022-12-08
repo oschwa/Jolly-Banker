3d9439587691b14ceb7675c1b69d923c2972a2d9
Parth Gupta <66531430+parthgu@users.noreply.github.com>
2022-12-07 16:48:54 -0800

    Create Banker.h

---
+++ Banker.h
@@ -0,0 +1,32 @@
+#ifndef BANKER_H
+#define BANKER_H
+
+#pragma once
+
+#include "AccountTree.h"
+// #include "TransactionReader.h"
+#include "Transaction.h"
+#include "Account.h"
+#include <queue>
+#include <fstream>
+#include <iostream>
+
+class Banker
+{
+private:
+	AccountTree accounts;
+	queue<Transaction> transactions;
+	// TransactionReader reader;
+
+public:
+	bool read(ifstream inputFile);
+	bool execute();
+	bool openAccount(std::string firstName, std::string lastName, string ID);
+	bool viewHistory(const Account &a);
+	bool transferFunds(Account &a, int fund1, int fund2);
+	bool transferFunds(Account &a, int fundA, Account &b, int fundB, int amount);
+	bool deposit(Account &a, int fund, int amount);
+	bool withdraw(Account &a, int fund, int amount);
+};
+
+#endif
