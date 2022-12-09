52d0eec600da4a0509101733af5f3bcca0c882d1
Parth Gupta <gparth02@hotmail.com>
2022-12-09 13:35:37 -0800

    Added comments for functions in Fund.h

--- Fund.h
+++ Fund.h
@@ -29,18 +29,75 @@ public:
 	~Fund();
 
 	// setter / getters
+
+	/**
+	 * getBalance Returns an integer representing the overall balance in the fund
+	 *
+	 * @pre	Fund is not NULL
+	 * @post Returns fund balance (int)
+	 */
 	int getBalance() const;
+
+	/**
+	 * getFundID Returns a char representing the fund ID (0 - 9)
+	 *
+	 * @pre Fund is not NULL
+	 * @post Returns the fund ID (char)
+	 */
 	char getFundID() const;
+
+	/**
+	 * getName Returns a string representing the fund name
+	 *
+	 * @pre Fund is not NULL
+	 * @post Returns the fund name (string)
+	 */
 	std::string getName() const;
+
+	/**
+	 * getHistory returns vector of Transaction objects representing the transaction history of the fund
+	 *
+	 * @pre Fund is not NULL
+	 * @post Transaction history is returned (vector<Transaction>)
+	 */
 	vector<Transaction> getHistory();
 
 	// utility methods
+
+	/**
+	 * add Adds amountToAdd to the balance of the fund
+	 *
+	 * @param amountToAdd the integer amount to add to fund balance
+	 * @post False if amountToAdd is negative
+	 * @post True if amountToAdd is positive and succesfully added to balance
+	 */
 	bool add(int amountToAdd);
+
+	/**
+	 * withdraw Removes amountToWithdraw from balance
+	 *
+	 * @param amountToWithdraw Subtracts amountToWithdraw from fund balance
+	 * @post false if balance does not have enough funds
+	 * @post true if amountToWithdraw successfully withdrawn from fund balance
+	 */
 	bool withdraw(int amountToWithdraw);
+
+	/**
+	 * addTransaction adds trans to the transaction history of the fund
+	 *
+	 * @param trans Transaction to be added to fund history
+	 * @post true if transaction successfully added to history
+	 */
 	bool addTransaction(Transaction trans);
+
+	/**
+	 * displayHistory Prints out transaction history of fund to the console
+	 *
+	 * @post Transaction history printed to console
+	 */
 	void displayHistory();
 
-	// operator overloads
+	// operator overloads (Compared via balance)
 	bool operator<(const Fund &other) const;
 	bool operator>(const Fund &other) const;
 	bool operator<=(const Fund &other) const;
