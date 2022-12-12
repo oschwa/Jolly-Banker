8f92d964c7117eccecc6f309e9b8cac559f13f35
oschwa <oschwa@uw.edu>
2022-12-12 15:21:58 -0800

    Header comments updated.

--- TransactionReader.h
+++ TransactionReader.h
@@ -14,32 +14,51 @@ private:
     */
     std::queue<Transaction> * readerQueue;
 public:
-    TransactionReader();
-    /*
-    Constructor for TransactionReader object.
-    readerQueue is assigned to transactionQueue here.
+    /**
+     * Default Constructor.
     */
+    TransactionReader();
+    /**
+     * Parameterized Constructor
+     * @param queue<Transaction> Address of Banker class queue.
+    */    
     TransactionReader(queue<Transaction> * transactionQueue);
-    /*
-    Method for reading file input.
-    Pre: Requires pointer to a Transaction queue.
-    Post: Returns boolean indicating success or failure.
+    /**
+     * Method for reading file input.
+     * @param std::string Name of file to be read.
+     * @post bool Indicates success or failure.
     */
     bool read(std::string file);
-    /*
-    Method for creating Transaction objects from file 
-    input.
-    Pre: Requires string for line from file.
-    Post: Returns a Transaction object reference.
+    /**
+     * Method for creating Transaction objects from file 
+     * input.
+     * @param std::string Line from input file.
     */
     void defineTransaction(std::string line);
-
+    /**
+     * Method for creating a Deposit or Withdrawal 
+     * Transaction object.
+     * @param std::string Line from input file.
+     * @post char Type of transaction.
+    */
     Transaction& defineDepositWithdrawal(std::string line, char type);
-
+    /**
+     * Method for creating an Account Open Transaction object.
+     * @param std::string Line from input file.
+     * @post char Type of transaction.
+    */
     Transaction& defineAccountOpen(std::string line, char type);
-
+    /**
+     * Method for creating a Transfer Transaction Object.
+     * @param std::string Line from input file.
+     * @post char Type of transaction.
+    */
     Transaction& defineFundTransfer(std::string line, char type);
-    
+    /**
+     * Method for creating a History Query Transaction Object.
+     * @param std::string Line from input file.
+     * @post char Type of transaction.
+    */
     Transaction& defineHistoryQuery(std::string line, char type);
 
 };
