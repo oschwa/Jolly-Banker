b207170c24490ea565e05b59183e44013263367b
oschwa <oschwa@uw.edu>
2022-12-11 19:37:44 -0800

    TransactionReader header comments updated.

--- TransactionReader.h
+++ TransactionReader.h
@@ -15,31 +15,55 @@ private:
     std::queue<Transaction> * readerQueue;
 public:
     TransactionReader();
-    /*
-    Constructor for TransactionReader object.
-    readerQueue is assigned to transactionQueue here.
+    /**
+     * Constructor for TransactionReader object.
+     * @param queue<Transaction> address of queue<Transaction>
+     * in Banker class.
     */
     TransactionReader(queue<Transaction> * transactionQueue);
-    /*
-    Method for reading file input.
-    Pre: Requires pointer to a Transaction queue.
-    Post: Returns boolean indicating success or failure.
+    /**
+     * Method for reading file input.
+     * @param std::string Name of file to be read.
+     * @post Returns bool indicating success or failue.
     */
     bool read(std::string file);
-    /*
-    Method for creating Transaction objects from file 
-    input.
-    Pre: Requires string for line from file.
-    Post: Returns a Transaction object reference.
+    /**
+     * Method for creating Transaction objects from file.
+     * @param std::string Line from file to be read.
+     * @post void.
     */
     void defineTransaction(std::string line);
-
+    /**
+     * Method for creating a Deposit / Withdrawl Transaction
+     * Object.
+     * @param std::string Line from file to be read.
+     * @param char Letter representing type of transaction.
+     * @post Returns Transaction object reference.
+    */
     Transaction& defineDepositWithdrawal(std::string line, char type);
-
+    /**
+     * Method for creating an Account Open Transaction
+     * Object.
+     * @param std::string Line from file to be read.
+     * @param char Letter representing type of transaction.
+     * @post Returns Transaction object reference.
+    */
     Transaction& defineAccountOpen(std::string line, char type);
-
+    /**
+     * Method for creating a Transfer Transaction
+     * Object.
+     * @param std::string Line from file to be read.
+     * @param char Letter representing type of transaction.
+     * @post Returns Transaction object reference.
+    */
     Transaction& defineFundTransfer(std::string line, char type);
-    
+    /**
+     * Method for creating a History Query Transaction
+     * Object.
+     * @param std::string Line from file to be read.
+     * @param char Letter representing type of transaction.
+     * @post Returns Transaction object reference.
+    */
     Transaction& defineHistoryQuery(std::string line, char type);
 
 };
